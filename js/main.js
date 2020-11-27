87e4d851bebf9c530c4c43b0b55e381396b1f96b
SHAON <saonroy15@gmail.com>
2020-11-27 20:37:58 +0600

    Complete The Back To Top

--- js/main.js
+++ js/main.js
@@ -113,3 +113,15 @@ $('.review-slider').slick({
         }
     ]
 });
+ // Back to top button
+ $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function () {
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    return false;
+});
